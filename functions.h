f64bce2b2a6a86ca778b10a8d406d25e7caebea9
lucasnprog <lucasnprog@gmail.com>
2023-11-03 15:20:37 -0300

    modified:   .vscode/settings.json 	new file:   database.c 	new file:   functions.c 	new file:   functions.h 	modified:   main.c 	new file:   output/main.exe 	new file:   output/structs.exe 	new file:   sqlite3.c 	new file:   sqlite3.h 	new file:   sqlite3.lib 	new file:   structs.h

---
+++ functions.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void addGerente(char name[], char cpf[], char password[]);
+
+#endif
---
+++ structs.h
@@ -0,0 +1,12 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+struct  Manager
+{
+    char name[100];
+    char cpf[12];
+    char password[50];
+} ;
+
+
+#endif
